4b062dc7a535542e287b87031cf68ece995c1fa5
dion <denniser8@gmail.com>
2025-06-05 16:20:46 +0300

    Fix downloader

--- download_images.py
+++ download_images.py
@@ -2,12 +2,11 @@
 """
 Main entry point for the Image Downloader tool
 """
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
-
-
 import sys
 import os
 from tools.cli import main
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
 
 if __name__ == '__main__':
     main() 
